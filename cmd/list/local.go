ec5924478164448ddaf29eb8dae6dc32a4c2074f
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-03-09 12:28:29 +0100

    Added local list

--- cmd/list/local.go
+++ cmd/list/local.go
@@ -1,14 +1,43 @@
 package list
 
 import (
+	"duckdb-version-manager/config"
+	"duckdb-version-manager/models"
+	"duckdb-version-manager/utils"
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+func printVersions(versions []models.Tuple[string, string]) {
+	fmt.Println("Installed DuckDB versions:")
+	for _, t := range versions {
+		padding := 8 - len(fmt.Sprintf("%v", t.First))
+		if padding < 1 {
+			padding = 1
+		}
+
+		fmt.Printf("  %-*v %v\n", padding+len(fmt.Sprintf("%v", t.First)), t.First, t.Second)
+	}
+}
 
 var LocalCmd = &cobra.Command{
 	Use:   "local",
 	Short: "List locally installed DuckDB versions",
 	Run: func(cmd *cobra.Command, args []string) {
 		// List the locally installed DuckDB versions
+		entries, err := os.ReadDir(config.VersionDir)
+		if err != nil {
+			utils.ExitWithError(err)
+		}
 
+		versions := make([]models.Tuple[string, string], 0)
+		for _, e := range entries {
+			if !e.IsDir() {
+				versions = append(versions, models.Tuple[string, string]{First: e.Name(), Second: config.VersionDir + "/" + e.Name()})
+			}
+		}
+
+		printVersions(versions)
 	},
 }
---
+++ models/tuple.go
@@ -0,0 +1,6 @@
+package models
+
+type Tuple[T1 any, T2 any] struct {
+	First  T1
+	Second T2
+}
