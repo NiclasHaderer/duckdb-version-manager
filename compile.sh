fb757300e70bab4b2021ccf42ec609ea795b0879
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-04-12 18:16:56 +0200

    Improved installation script

--- compile.sh
+++ compile.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
 # Get version from git
 VERSION=$1
 
 # Check if version is set
 if [ -z "$VERSION" ]; then
-    echo "Version not set"
-    exit 1
+	echo "Version not set"
+	exit 1
 fi
-
 
 # Variables
 BINARY_NAME="duckman"
@@ -24,19 +22,19 @@ mkdir -p $OUTPUT_DIR
 # Loop through architectures
 # shellcheck disable=SC2068
 for i in ${!GOOS_ARRAY[@]}; do
-    GOOS=${GOOS_ARRAY[$i]}
-    GOARCH=${GOARCH_ARRAY[$i]}
+	GOOS=${GOOS_ARRAY[$i]}
+	GOARCH=${GOARCH_ARRAY[$i]}
 
-    OUTPUT_NAME="$OUTPUT_DIR/$BINARY_NAME-$GOOS-$GOARCH"
+	OUTPUT_NAME="$OUTPUT_DIR/$BINARY_NAME-$GOOS-$GOARCH"
 
-    # Cross-compile
-    env GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags "-X 'duckdb-version-manager/cmd.version=$VERSION'" -o "$OUTPUT_NAME"
+	# Cross-compile
+	env GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags "-X 'duckdb-version-manager/cmd.version=$VERSION'" -o "$OUTPUT_NAME"
 
-    # Check if cross-compilation was successful
-    if [ $? -eq 0 ]; then
-        echo "Successfully compiled $BINARY_NAME for $GOOS $GOARCH"
-    else
-        echo "Error compiling $BINARY_NAME for $GOOS $GOARCH"
-        exit 1
-    fi
+	# Check if cross-compilation was successful
+	if [ $? -eq 0 ]; then
+		echo "Successfully compiled $BINARY_NAME for $GOOS $GOARCH"
+	else
+		echo "Error compiling $BINARY_NAME for $GOOS $GOARCH"
+		exit 1
+	fi
 done
--- install.sh
+++ install.sh
@@ -2,50 +2,111 @@
 
 URL="https://raw.githubusercontent.com/NiclasHaderer/duckdb-version-manager/main/versions/latest-vm.json"
 
-if ! command -v jq &> /dev/null; then
-    echo "jq could not be found. Please install jq to proceed."
-    exit 1
-fi
+function download() {
+	if ! command -v jq &>/dev/null; then
+		echo "jq could not be found. Please install jq to proceed."
+		exit 1
+	fi
 
-DOWNLOAD_DIR="$HOME/.local/bin"
-mkdir -p "${DOWNLOAD_DIR}"
+	DOWNLOAD_DIR="$HOME/.local/bin"
+	mkdir -p "${DOWNLOAD_DIR}"
 
-JSON_CONTENT=$(curl -sL "${URL}")
+	JSON_CONTENT=$(curl -sL "${URL}")
 
-OS="$(uname -s)"
-ARCH="$(uname -m)"
+	OS="$(uname -s)"
+	ARCH="$(uname -m)"
 
-case "${ARCH}" in
-    x86_64) ARCH="ArchitectureX86" ;;
-    arm64) ARCH="ArchitectureArm64" ;;
-    *) echo "Unsupported architecture: ${ARCH}"; exit 2 ;;
-esac
+	case "${ARCH}" in
+	x86_64) ARCH="ArchitectureX86" ;;
+	arm64) ARCH="ArchitectureArm64" ;;
+	*)
+		echo "Unsupported architecture: ${ARCH}"
+		exit 2
+		;;
+	esac
 
-case "${OS}" in
-    Linux) OS_KEY="PlatformLinux" ;;
-    Darwin) OS_KEY="PlatformMac" ;;
-    *) echo "Unsupported OS: ${OS}"; exit 3 ;;
-esac
+	case "${OS}" in
+	Linux) OS_KEY="PlatformLinux" ;;
+	Darwin) OS_KEY="PlatformMac" ;;
+	*)
+		echo "Unsupported OS: ${OS}"
+		exit 3
+		;;
+	esac
 
-DOWNLOAD_URL=$(echo "${JSON_CONTENT}" | jq -r ".platforms.${OS_KEY}.${ARCH}.downloadUrl")
+	DOWNLOAD_URL=$(echo "${JSON_CONTENT}" | jq -r ".platforms.${OS_KEY}.${ARCH}.downloadUrl")
 
-if [ "${DOWNLOAD_URL}" == "null" ] || [ -z "${DOWNLOAD_URL}" ]; then
-    echo "Failed to find a valid download URL for your platform."
-    exit 4
-fi
+	if [ "${DOWNLOAD_URL}" == "null" ] || [ -z "${DOWNLOAD_URL}" ]; then
+		echo "Failed to find a valid download URL for your platform."
+		exit 4
+	fi
 
-echo "Downloading from ${DOWNLOAD_URL}..."
-curl -sL "${DOWNLOAD_URL}" -o "${DOWNLOAD_DIR}/duckman"
-echo "Download complete. duckman is now available in ${DOWNLOAD_DIR}/duckman"
+	echo "Downloading from ${DOWNLOAD_URL}..."
+	curl -sL "${DOWNLOAD_URL}" -o "${DOWNLOAD_DIR}/duckman"
+	echo "Download complete. duckman is now available in ${DOWNLOAD_DIR}/duckman"
 
-chmod +x "${DOWNLOAD_DIR}/duckman"
+	chmod +x "${DOWNLOAD_DIR}/duckman"
+}
 
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-    echo ""
-    echo "$HOME/.local/bin is not in PATH."
-    echo "Add it to your PATH by adding the following line to your shell's configuration file:"
-    echo "export PATH=\"\$HOME/.local/bin:\$PATH\""
-fi
+function appendIfNotPresent() {
+	file="$1"
+	line="$2"
 
-echo "To get autocomplete for duckman, add the following line to your shell's configuration file:"
-echo "eval \"\$(duckman completion <zsh|bash|fish>)\""
+	if ! grep -qF "$line" "$file"; then
+		echo "$line" >>"$file"
+	fi
+}
+
+function setupShells() {
+	echo "Setting up shell completions and PATH..."
+
+	# Bash
+	bash_rc="$HOME/.bashrc"
+	if [ -f "bash_rc" ]; then
+		echo "Configuring bash"
+		appendIfNotPresent "$bash_rc" "export PATH=\"\$HOME/.local/bin:\$PATH\""
+		appendIfNotPresent "$bash_rc" "eval \"\$(duckman completion bash)\""
+	fi
+
+	# Zsh
+	if [ -f "$HOME/.zshrc" ]; then
+		echo "Configuring zsh"
+		appendIfNotPresent "$HOME/.zshrc" "export PATH=\"\$HOME/.local/bin:\$PATH\""
+		appendIfNotPresent "$HOME/.zshrc" "eval \"\$(duckman completion zsh)\""
+	fi
+
+	# Fish
+	if [ -f "$HOME/.config/fish/config.fish" ]; then
+		echo "Configuring fish"
+		appendIfNotPresent "$HOME/.config/fish/config.fish" "set -x PATH \$HOME/.local/bin \$PATH"
+		appendIfNotPresent "$HOME/.config/fish/config.fish" "duckman completion fish | source"
+	fi
+}
+
+function printShellHelp() {
+	if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+		echo ""
+		echo "$HOME/.local/bin is not in PATH."
+		echo "Add it to your PATH by adding the following line to your shell's configuration file:"
+		echo "export PATH=\"\$HOME/.local/bin:\$PATH\""
+	fi
+	echo "To get autocomplete for duckman, add the following line to your shell's configuration file:"
+	echo "eval \"\$(duckman completion <zsh|bash|fish>)\""
+}
+
+download
+
+echo "Do you want duckman to setup autocomplete and PATH for you?"
+select yn in "Yes" "No"; do
+	case $yn in
+	Yes)
+		setupShells
+		break
+		;;
+	No)
+		printShellHelp
+		break
+		;;
+	*) echo "Answer with 1 or 2" ;;
+	esac
+done
