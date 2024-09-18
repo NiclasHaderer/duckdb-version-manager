da04732501d4c3fcd875492abf96dbcf47c2307b
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-09-18 11:25:57 +0200

    Re-enabled nightly versions

--- scripts/nightly.py
+++ scripts/nightly.py
@@ -4,23 +4,23 @@ NIGHTLY_RELEASE: Release = {
     "version": "nightly",
     "name": "nightly",
     "platforms": {
-        # "PlatformWindows": {
-        #     "ArchitectureX86": {
-        #         "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-windows.zip",
-        #     },
-        # },
-        # "PlatformLinux": {
-        #     "ArchitectureX86": {
-        #         "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-linux.zip",
-        #     },
-        #     "ArchitectureArm64": {
-        #         "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-linux-aarch64.zip",
-        #     },
-        # },
-        # "PlatformMac": {
-        #     "ArchitectureUniversal": {
-        #         "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-osx.zip",
-        #     },
-        # },
+        "PlatformWindows": {
+            "ArchitectureX86": {
+                "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-windows.zip",
+            },
+        },
+        "PlatformLinux": {
+            "ArchitectureX86": {
+                "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-linux.zip",
+            },
+            "ArchitectureArm64": {
+                "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-linux-aarch64.zip",
+            },
+        },
+        "PlatformMac": {
+            "ArchitectureUniversal": {
+                "downloadUrl": "https://artifacts.duckdb.org/latest/duckdb-binaries-osx.zip",
+            },
+        },
     },
 }
